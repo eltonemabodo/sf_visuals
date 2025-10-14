aa74af66c500bf72afcf0ef2461a58610a6609ac
Eltone Mabodo <eltone.mabodo@gmail.com>
2025-10-14 14:54:04 +0700

    Minor changes

--- code/visualisations.R
+++ code/visualisations.R
@@ -349,3 +349,92 @@ ggsave(
   width = 10, height = 7, dpi = 300
 )
 ########################################################################################################
+
+# Schools vs SFP Schools Graph
+
+schools_graph <- schools_perc_data %>% 
+  ggplot(aes(x = year_date, y = number, group = type, fill = type)) +
+  geom_bar(
+    stat = "identity", 
+    linewidth = 0.9, 
+    colour = "white", 
+    position = "stack"
+  ) +
+  scale_x_date(
+    expand = c(0, 0),  # Add some space on the right
+    breaks = label_cumulative_students_df$year_date,  # only every 3rd date
+    labels = label_cumulative_students_df$year        # show "1999-00", "2002-03", etc.
+  ) +
+  scale_y_continuous(
+    labels = scales::comma,  # Format y-axis with commas
+    expand = c(0, 0),  # Remove extra space on the y-axis
+    limits = c(0, 15000),  # Set y-axis limits
+    breaks = c(1000, 5000, 10000, 15000)
+  ) +
+  scale_fill_manual(
+    values = c(
+      "sfp_sch" = "#FCC61D",
+      "nat_schools" = "#3C467B"
+    ),
+    labels = c("Total Schools", "SFP Schools")
+  )+
+  annotate(
+    "rect",
+    xmin = as.Date("2011-04-01"), xmax = as.Date("2014-04-01"),
+    ymin = 0, ymax = 15000,
+    alpha = 0.2,
+    fill = "#3C467B"
+  ) + 
+  labs(
+    title = "Schools Benefiting from School Feeding in Cambodia",
+    subtitle = "The number of schools benefiting from school feeding programs has increased significantly since 1999,\naveraging ~3,000 beneficiaries per year.",
+    x = "School Calendar Year",
+    y = "Number of Schools",
+    fill = "Program Type",
+    caption = "**Source:** WFP Cambodia School Feeding Data (1999-2025)<br><br>**Visualisation:** RAM Unit"
+  ) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(size = 18,
+                              family = "opensans_extrabold",
+                              margin = margin(b = 10),
+                              colour = "#3C467B"),
+    plot.title.position = "plot",
+    plot.subtitle = element_text(size = 15,
+                                 family = "opensans_light",
+                                 face = "italic",
+                                 margin = margin(b = 20),
+                                 colour = "#3C467B"),
+    plot.caption = element_markdown(size = 12,
+                                    family = "opensans_light",
+                                    margin = margin(t = 20),
+                                    hjust = 0,
+                                    colour = "#3C467B"),
+    plot.caption.position = "plot",
+    axis.title.x = element_text(size = 14, family = "opensans_extrabold",
+                                margin = margin(t = 15),
+                                colour = "#3C467B"),
+    axis.title.y = element_text(size = 14, family = "opensans_extrabold",
+                                margin = margin(r = 10),
+                                colour = "#3C467B"),
+    axis.text.y = element_text(size = 12, family = "opensans_extrabold", colour = "#3C467B"),
+    axis.text.x = element_text(size = 12, family = "opensans_extrabold", colour = "#3C467B", hjust = 0.5),
+    axis.ticks.x = element_line(colour = "#3C467B"),
+    axis.ticks.y = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_line(colour = "#C5C7BC", linetype = 3),
+    legend.text = element_text(size = 12, family = "opensans_light", colour = "#3C467B"),
+    legend.key.size = unit(0.4, "cm"),
+    legend.position = "bottom",
+    legend.title = element_blank()
+  )
+
+
+# Save the schools graph
+ggsave(
+  filename = here::here("figures", "schools_benefiting_school_feeding.png"),
+  plot = schools_graph,
+  bg = "white",
+  width = 10, height = 7, dpi = 300
+)
+########################################################################################################
