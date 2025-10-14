738ebd3fbd2ec415091f41b21075b68ae6ac4ce7
Eltone Mabodo <eltone.mabodo@gmail.com>
2025-10-14 10:38:05 +0700

    Added the code for the next graph

--- code/data_processing.R
+++ code/data_processing.R
@@ -62,3 +62,15 @@ label_cumulative_students_df <- cumulative_students_data %>%
   slice(seq(1, n(), by = 10))
 #############################################################################################
 
+
+beneficiaries_perc_data <- trend_data %>% 
+  select(year,nat_students, total_students) %>% 
+  mutate(
+    perc =  total_students/nat_students  * 100,
+    start_year = substr(year, 1, 4),
+    year_date = ymd(paste0(start_year, "-10-01"))
+  ) 
+
+label_beneficiaries_perc_df <- beneficiaries_perc_data %>%
+  slice(seq(1, n(), by = 10))
+#############################################################################################
