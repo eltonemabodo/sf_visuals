8fbf94cacea0f51fcce77506f33c9f3a9fb3fd55
Eltone Mabodo <eltone.mabodo@gmail.com>
2025-10-14 10:03:35 +0700

    The second graph is complete...

--- code/data_processing.R
+++ code/data_processing.R
@@ -29,11 +29,36 @@ label_df <- schools_data %>% slice(seq(1, n(), by =10))
 ## Students trend data
 
 students_data <- trend_data %>% 
-  select(year, sm_hgsf_students,sm_students, students_hgsf,  thr_students, sm_hgsf_students) %>%
+  select(year, wfp_students, nhgsf_students, total_students) %>%
+  pivot_longer(
+    cols = -year,
+    names_to = "type",
+    values_to = "students"
+  ) %>% 
+  mutate(
+    start_year = substr(year, 1, 4),
+    year_date = ymd(paste0(start_year, "-10-01")), 
+    students = if_else(students == 0, NA_real_, students)
+  ) %>% 
+  mutate(type = factor(
+    type,
+    levels = c("wfp_students", "nhgsf_students", "total_students")
+  ))
+
+label_students_df <- students_data %>% slice(seq(1, n(), by = 15))
+
+#############################################################################################
+
+## Cummulative Students Data
+
+cumulative_students_data <- trend_data %>%
+  select(year, cumulative_students) %>%
   mutate(
     start_year = substr(year, 1, 4),
     year_date = ymd(paste0(start_year, "-10-01"))
-  ) %>% 
-  arrange(year_date)
+  )
 
-label_students_df <- students_data %>% slice(seq(1, n(), by = 10))
+label_cumulative_students_df <- cumulative_students_data %>%
+  slice(seq(1, n(), by = 10))
+#############################################################################################
+
--- code/visualisations.R
+++ code/visualisations.R
@@ -73,32 +73,206 @@ sf_trend_graph <- ggplot(schools_data, aes(x = year_date,
 ggsave(
   filename = here::here("figures", "school_feeding_trend.png"),
   plot = sf_trend_graph,
+  bjg = "white",
   width = 10, height = 7, dpi = 300
 )
 
 
 ############################################################################################
 
-students_data %>% 
-  pivot_longer(
-    cols = -year,
-    names_to = "type",
-    values_to = "students"
-  ) %>% 
-  filter(type != "sm_students") %>%
-  ggplot(aes(x = year, y = students, colour = type, fill = type, group = type)) +
-  geom_line(size = 1.2) +
-  #geom_point(size = 3) +
+students_graph <- students_data %>% 
+  filter(type != "total_students") %>%  # Exclude total_students for clarity
+  #filter(type != "sm_students") %>%
+  ggplot(aes(x = year_date, y = students, colour = type, fill = type, group = type)) +
+  geom_line(size = 1, linetype = 3, show.legend = FALSE) +
+  #geom_point(size = 2, shape = 21, color = "white", stroke = 1) +
   geom_area(
     position = "identity",
-    alpha = 0.3,
+    alpha = 0.8,
     show.legend = TRUE
+  ) + 
+  scale_x_date(
+    expand = c(0, 0.5),  # Add some space on the right
+    breaks = label_students_df$year_date,  # only every 3rd date
+    labels = label_students_df$year        # show "1999-00", "2002-03", etc.
+  ) +
+  scale_y_continuous(
+    labels = scales::comma,  # Format y-axis with commas
+    expand = c(0, 0),  # Remove extra space on the y-axis
+    limits = c(0, 800000),  # Set y-axis limits
+    breaks = c(200000, 300000, 400000, 500000, 600000, 700000, 800000)) +
+  scale_color_manual(
+    values = c(
+      "wfp_students" =  "#3C467B",
+      "nhgsf_students" = "#FCC61D",
+      "total_students" = "#954C2E"
+    ),
+    labels = c("WFP Students", "NHGSF Students", "Total Students")
+  ) +
+  scale_fill_manual(
+    values = c(
+      "wfp_students" = "#3C467B",
+      "nhgsf_students" = "#FCC61D",
+      "total_students" = "#954C2E"
+    ),
+    labels = c("WFP Students", "NHGSF Students", "Total Students")
+  ) +
+  annotate(
+    "curve",
+    x = as.Date("2006-10-01"), y = 700000,
+    xend = as.Date("2005-10-01"), yend = 600000,
+    curvature = 0.2,
+    arrow = arrow(length = unit(0.2, "cm"), type = "closed"),
+    colour = "#3C467B",
+    size = 0.3,
+    alpha = 0.8
+  ) + 
+  annotate(
+    "text",
+    x = as.Date("2006-11-20"), y = 700000,
+    label = "Students Benefiting\npeaked at ~590,000\nin 2006/5 school year",
+    lineheight = 0.9,
+    colour = "#3C467B",
+    family = "opensans_extrabold",
+    size = 4,
+    hjust = 0
+  ) +
+  annotate(
+    "curve",
+    xend = as.Date("2017-06-01"), y = 10000,
+    x = as.Date("2019-08-01"), yend = 150000,
+    curvature = 0.2,
+    arrow = arrow(length = unit(0.2, "cm"), type = "closed", ends = "first"),
+    colour = "#FCC61D",
+    size = 0.3
+  ) +
+  annotate(
+    "text",
+    x = as.Date("2017-03-01"), y = 150000,
+    label = "Handover started in 2019/20,\nwith ~52,000 students\nenrolled in the NHGSF",
+    lineheight = 0.9,
+    colour = "#FCC61D",
+    family = "opensans_extrabold",
+    size = 4,
+    hjust = 1
+  ) +
+  labs(
+    title = "Students Benefiting from School Feeding in Cambodia",
+    subtitle = "The number of students benefiting from school feeding programs has increased significantly since 1999,\naveraging ~330,000 beneficiaries per year.",
+    x = "School Calendar Year",
+    y = "Number of Students",
+    fill = "Program Type",
+    colour = "Program Type",
+    caption = "**Source:** WFP Cambodia School Feeding Data (1999-2025)<br><br>**Visualisation:** RAM Unit"
+  ) +
+  theme_minimal() + 
+  theme(
+    plot.title = element_text(size = 18,
+                              family = "opensans_extrabold",
+                              margin = margin(b = 10),
+                              colour = "#3C467B"),
+    plot.title.position = "plot",
+    plot.subtitle = element_text(size = 15,
+                                 family = "opensans_light",
+                                 face = "italic",
+                                 margin = margin(b = 20),
+                                 colour = "#3C467B"),
+    plot.caption = element_markdown(size = 12,
+                                    family = "opensans_light",
+                                    margin = margin(t = 20),
+                                    hjust = 0,
+                                    colour = "#3C467B"),
+    plot.caption.position = "plot",
+    axis.title.x = element_text(size = 14, family = "opensans_extrabold",
+                                margin = margin(t = 15),
+                                colour = "#3C467B"),
+    axis.title.y = element_text(size = 14, family = "opensans_extrabold",
+                                margin = margin(r = 10),
+                                colour = "#3C467B"),
+    axis.text.y = element_text(size = 12, family = "opensans_extrabold", colour = "#3C467B"),
+    axis.text.x = element_text(size = 12, family = "opensans_extrabold", colour = "#3C467B", hjust = 0.95),
+    axis.ticks.x = element_line(colour = "#3C467B"),
+    axis.ticks.y = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_line(colour = "#C5C7BC", linetype = 3),
+    legend.text = element_text(size = 12, family = "opensans_light", colour = "#3C467B"),
+    legend.key.size = unit(0.4, "cm"),
+    legend.position = "bottom",
+    legend.title = element_blank()
   )
+# Save the students graph
+ggsave(
+  filename = here::here("figures", "students_benefiting_school_feeding.png"),
+  plot = students_graph,
+  bg = "white",
+  width = 10, height = 7, dpi = 300
+)
 
+########################################################################################################
 
+# Cumulative Students Graph
 
+cummulative_students_graph <- cumulative_students_data %>% 
+  ggplot(aes(x = year_date, y = cumulative_students)) +
+  geom_line(size = 1, colour = "#3C467B", linetype = 3) +
+  geom_point(size = 3, colour = "#3C467B") +
+  scale_x_date(
+    expand = c(0, 0),  # Add some space on the right
+    breaks = label_cumulative_students_df$year_date,  # only every 3rd date
+    labels = label_cumulative_students_df$year        # show "1999-00", "2002-03", etc.
+  ) +
+  scale_y_continuous(
+    labels = scales::comma,  # Format y-axis with commas
+    expand = c(0, 0)  # Remove extra space on the y-axis
+  ) +
+  labs(
+    title = "Cumulative Students Benefiting from School Feeding in Cambodia",
+    subtitle = "The cumulative number of students benefiting from school feeding programs has reached over\n4 million since the program's inception in 1999.",
+    x = "School Calendar Year",
+    y = "Cumulative Number of Students",
+    caption = "**Source:** WFP Cambodia School Feeding Data (1999-2025)<br><br>**Visualisation:** RAM Unit"
+  ) +
+  theme_minimal() + 
+  theme(
+    plot.title = element_text(size = 18,
+                              family = "opensans_extrabold",
+                              margin = margin(b = 10),
+                              colour = "#3C467B"),
+    plot.title.position = "plot",
+    plot.subtitle = element_text(size = 15,
+                                 family = "opensans_light",
+                                 face = "italic",
+                                 margin = margin(b = 20),
+                                 colour = "#3C467B"),
+    plot.caption = element_markdown(size = 12,
+                                    family = "opensans_light",
+                                    margin = margin(t = 20),
+                                    hjust = 0,
+                                    colour = "#3C467B"),
+    plot.caption.position = "plot",
+    axis.title.x = element_text(size = 14, family = "opensans_extrabold",
+                                margin = margin(t = 15),
+                                colour = "#3C467B"),
+    axis.title.y = element_text(size = 14, family = "opensans_extrabold",
+                                margin = margin(r = 10),
+                                colour = "#3C467B"),
+    axis.text = element_text(size = 12, family = "opensans_extrabold", colour = "#3C467B"),
+    axis.ticks.x = element_line(colour = "#3C467B"),
+    axis.ticks.y = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_line(colour = "#C5C7BC", linetype = 3),
+    legend.text = element_text(size = 12, family = "opensans_light", colour = "#3C467B"),
+    legend.key.size = unit(0.4, "cm"),
+    legend.position = "bottom",
+    legend.title = element_blank()
+  )
+# Save the cumulative students graph
 
-
+ggsave(
+  filename = here::here("figures", "cumulative_students_benefiting_school_feeding.png"),
+  plot = cummulative_students_graph,
+  width = 10, height = 7, dpi = 300
+)
 
 
 
